1c580ebe76c9493958a148f99135bdda49c34ad5
James Taylor <jamestaylr@gmail.com>
2017-11-06 23:15:12 -0500

    Report device statistics to Postgres database

--- src/dstat/benchmark.cr
+++ src/dstat/benchmark.cr
@@ -1,5 +1,32 @@
+require "db"
+require "pg"
+
 module DStat
   class Benchmark
+    def run
+      now = Time.now
+      config = Utils.config["database"].as(Hash)
+
+      connection = [
+        "postgres://#{config["user"]}:#{config["password"]}",
+        "@localhost/stats",
+      ].join
+
+      DB.open connection do |db|
+        [
+          {"cpu", get_load},
+          {"memory", get_mem},
+          {"disk", get_disk},
+        ].each do |type, value|
+          query = [
+            "INSERT INTO metrics (datetime, type, value)",
+            "VALUES ($1, $2, $3)",
+          ].join
+          db.exec query, now, type, value
+        end
+      end
+    end
+
     private def get_disk
       io = IO::Memory.new
       Process.run("df", {"/", "--output=target,avail"}, output: io)
--- src/dstat/cli.cr
+++ src/dstat/cli.cr
@@ -3,6 +3,9 @@ module DStat
     def run
       OptionParser.parse! do |parser|
         parser.banner = "Usage: dstat [arguments]"
+        parser.on("-r", "Check machine") {
+          DStat::Benchmark.new.run
+        }
         parser.invalid_option do |option|
           puts "No option found for #{option}"
           puts parser
