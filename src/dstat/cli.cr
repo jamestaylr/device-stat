c6da37d0355fb5bf938efeefd8293bf8622b081e
James Taylor <jamestaylr@gmail.com>
2017-11-26 17:56:12 -0500

    Add basic stat query functions

--- src/dstat/cli.cr
+++ src/dstat/cli.cr
@@ -3,9 +3,14 @@ module DStat
     def run
       OptionParser.parse! do |parser|
         parser.banner = "Usage: dstat [arguments]"
+
         parser.on("-r", "Check machine") {
           DStat::Benchmark.new.run
         }
+        parser.on("-d", "Check machine") {
+          DStat::Daemon.new.run
+        }
+
         parser.invalid_option do |option|
           puts "No option found for #{option}"
           puts parser
--- src/dstat/daemon.cr
+++ src/dstat/daemon.cr
@@ -3,32 +3,47 @@ require "http/server"
 
 module DStat
   class Daemon
-    private def query_self
+    def self.database : DB::Database
+      config = Utils.config["database"].as(Hash)
+      connection = [
+        "postgres://#{config["user"]}:#{config["password"]}",
+        "@localhost/stats",
+      ].join
+
+      DB.open connection
+    end
+
+    private def query_host(hostname)
       result = {} of String => Float64
       target = Time.now - 1.hours
       config = Utils.config["database"].as(Hash)
-      daemon = Utils.config["daemon"].as(Hash)
 
       connection = [
         "postgres://#{config["user"]}:#{config["password"]}",
         "@localhost/stats",
       ].join
 
-      DB.open connection do |db|
-        [
-          "cpu",
-          "memory",
-          "disk",
-        ].each do |type|
-          query = [
-            "SELECT avg(value) FROM metrics",
-            "WHERE datetime > '#{target}' AND type='#{type}'",
-            "AND hostname=#{daemon["hostname"]}",
-          ].join(" ")
+      [
+        "cpu",
+        "memory",
+        "disk",
+      ].each do |type|
+        query = [
+          "SELECT avg(value) FROM metrics",
+          "WHERE datetime > '#{target}' AND type='#{type}'",
+          "AND hostname='#{hostname}'",
+        ].join(" ")
 
-          average = db.query_one(query, as: PG::Numeric).to_f
-          result[type] = average.round(2)
-        end
+        average = DStat::Daemon.database.query_one(query, as: PG::Numeric).to_f
+        result[type] = average.round(2)
+      end
+      result
+    end
+
+    def query_all
+      result = {} of String => Hash(String, Float64)
+      ["capstone0", "capstone1", "capstone2"].each do |h|
+        result[h] = query_host(h)
       end
       result
     end
@@ -38,7 +53,7 @@ module DStat
       port = config["port"].as(Int64).to_i32
       server = HTTP::Server.new(port) do |context|
         context.response.content_type = "text/plain"
-        context.response.print query_self.to_json
+        context.response.print query_all.to_json
       end
       server.listen
     end
