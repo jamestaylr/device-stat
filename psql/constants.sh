3522b355822dacaf40cef6813a52b6fd1c6d0399
James Taylor <jamestaylr@gmail.com>
2017-12-05 15:26:34 -0500

    Add setup scripts for node constants

---
+++ psql/constants.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Must install postgresql-client package
+
+PSQL_USER="capstone0";
+PSQL_IP="192.168.0.219";
+export PGPASSWORD='password';
+
+psql stats -U $PSQL_USER -h $PSQL_IP << EOH
+    INSERT INTO constants (type, value, hostname)
+    VALUES ('cpu', $(nproc), $(hostname));
+EOH
+
+psql stats << EOH
+    INSERT INTO constants (type, value, hostname)
+    VALUES (
+        'memory',
+        $(cat /proc/meminfo | grep 'MemTotal' | awk '{print $2}'),
+        $(hostname)
+    );
+EOH
+
+psql stats << EOH
+    INSERT INTO constants (type, value, hostname)
+    VALUES (
+        'disk',
+        $(df / --output=size | tail -n 1),
+        $(hostname)
+    );
+EOH
