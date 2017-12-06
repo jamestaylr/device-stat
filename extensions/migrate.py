8b47287d710e1e06e976c47a34373c784bd07928
James Taylor <jamestaylr@gmail.com>
2017-12-05 22:35:33 -0500

    Simplify unhealthy node migration logic

--- extensions/migrate.py
+++ extensions/migrate.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
 import os_client_config
 import requests
 import json
 import logging
+from collections import namedtuple
 
 
-THRESHOLD = 0.8
+Node = namedtuple('Node', 'hostname value')
+UPPER_THRESHOLD = 0.8
+LOWER_THRESHOLD = 0.5
 
 
 def get_dstat():
@@ -27,29 +31,31 @@ def random_evict(src, dest):
     s.live_migrate(host=dest, block_migration=True)
 
 
+def calculate_pressure(data):
+    for host, metrics in data.items():
+        for metric, h in metrics.items():
+            data[host][metric]['pressure'] = h['expected'] / h['constant']
+            logging.info('[{}] Metric {} at {} threshold'.format(
+                host,
+                metric,
+                data[host][metric]['pressure']
+            ))
+    return data
+
+
+def should_evict(unhealthy, healthy):
+    return unhealthy.value > UPPER_THRESHOLD and \
+        healthy.value < LOWER_THRESHOLD
+
+
 def main():
-    eviction_target = None
-    healthy_target = {
-            'name': None,
-            'health': 1,
-        }
+    data = calculate_pressure(get_dstat())
+    cpu_list = [Node(v, k['cpu']['pressure']) for v, k in data.items()]
+    healthy = min(cpu_list, key=lambda x: x.value)
+    unhealthy = max(cpu_list, key=lambda x: x.value)
 
-    for host, state in get_dstat().items():
-        health = []
-        for metric, values in state.items():
-            pressure = values['expected'] / values['constant']
-            health.append(pressure)
-            logging.info('Metric {} at {} threshold'.format(metric, pressure))
-        avg = sum(health) / len(health)
-        if avg > THRESHOLD:
-            eviction_target = host
-
-        if avg < healthy_target['health']:
-            healthy_target['name'] = host
-            healthy_target['health'] = avg
-
-    if eviction_target is not None:
-        random_evict(eviction_target, healthy_target['name'])
+    if should_evict(unhealthy, healthy):
+        random_evict(unhealthy.hostname, healthy.hostname)
 
 
 if __name__ == "__main__":
